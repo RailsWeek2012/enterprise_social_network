7a5b10e524895f161782527586ba1223ea131f01
Christian Brandt <christianbrandt90@gmail.com>
2012-07-23 10:42:02 +0200

    implemented group invitation

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -4,7 +4,7 @@ $(function() {
             $('#invite_to_group option[value="'+data.user.id+'"]').remove();
             $('#invite_to_group').prepend('<div class="alert alert-success">'+
                 '<button class="close" data-dismiss="alert">×</button>'+
-                '<strong>Success!</strong> '+data.user.first_name+' '+data.user.last_name+' has been invited to the group.'+
+                '<strong>'+data.user.first_name+' '+data.user.last_name+'</strong> has been invited to the group.'+
             '</div>');
         });
 });
