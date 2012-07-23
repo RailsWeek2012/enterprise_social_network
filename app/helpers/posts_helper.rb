e0b7985091be4080a41d1b610794485b8f8ac10a
Christian Brandt <christianbrandt90@gmail.com>
2012-07-23 11:51:19 +0200

    disable group posts in mainstream

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
 	def self.get_all_posts company_id
-		return Post.where('company_id = ?', company_id).order('created_at DESC')
+		return Post.where('company_id = ? AND group_id IS NULL', company_id).order('created_at DESC')
 	end
 end
