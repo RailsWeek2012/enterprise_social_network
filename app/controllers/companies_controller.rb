8438661657eb1d5406d9176cceb5d412a587606d
Christian Brandt <christianbrandt90@gmail.com>
2012-07-24 09:58:12 +0200

    fixed profile links. added some nav elements.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,7 +2,7 @@ class CompaniesController < ApplicationController
   # GET /companies
   # GET /companies.json
   def index
-    @companies = Company.all
+    @companies = Company.where('id > 0').order('name')
 
     respond_to do |format|
       format.html # index.html.erb
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,6 @@
 module UsersHelper
-	def back_to_users
-		link_to "Back", users_path, class: "btn"
-	end
-
 	def profile_link(user)
-		link_to user.full_name, user
+		link_to content_tag(:span, gravatar_image_tag(user.email, alt: user.full_name), class: "image")+" "+user.full_name, user, class: "profile-link"
 	end
 
 	def edit_profile(user)
